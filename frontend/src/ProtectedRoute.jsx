685108d2485f6f612d7f7d17b0a687bbbfd6aef8
locwa <94048557+locwa@users.noreply.github.com>
2025-10-03 20:02:07 +0800

    added ProtectedRoute.jsx

---
+++ frontend/src/ProtectedRoute.jsx
@@ -0,0 +1,7 @@
+import { Navigate } from "react-router";
+import { useAuth } from "./contexts/AuthContext";
+
+export default function ProtectedRoute({ children }) {
+    const { user } = useAuth();
+    return user ? children : <Navigate to="/login" replace />;
+}

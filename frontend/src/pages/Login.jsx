c7f603e1019cf3bd7e2ff4c7ee90fd5981d3fa64
locwa <94048557+locwa@users.noreply.github.com>
2025-10-03 22:01:46 +0800

    changed stuff

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
     }
 
     const handleLogin = () => {
-        login("Louis"); // fake login
+        login("User"); // fake login
         navigate("/home");
     };
 

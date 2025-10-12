4779a7f73252173c85e2168cd3c0637c86afc507
locwa <94048557+locwa@users.noreply.github.com>
2025-10-12 16:11:32 +0800

    added new scripts

---
+++ backend/seeders/20251012071116-demo-procurementlog.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const procurementLog = require('./data/procurement_log')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('ProcurementLog', procurementLog)
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('ProcurementLog', null, {})
+  }
+};

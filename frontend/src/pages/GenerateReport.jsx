6fba8ebf97a71a5cdefa00c5ca8fe311b5b46938
locwa <94048557+locwa@users.noreply.github.com>
2025-10-12 13:54:16 +0800

    removed useAuth

--- frontend/src/pages/GenerateReport.jsx
+++ frontend/src/pages/GenerateReport.jsx
@@ -1,11 +1,10 @@
 import DashboardTemplate from "../templates/DashboardTemplate.jsx";
 
 export default function Home() {
-    const { user } = useAuth()
     return (
         <DashboardTemplate>
             <h1>Generate Report</h1>
-            <h2>Welcome {user.name}!</h2>
+            <h2>Welcome</h2>
         </DashboardTemplate>
     );
 }
